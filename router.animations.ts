c89f4e71f973615d99ee138bf05082523f454006
Gustavus Shaw II <gshaw1997@gmail.com>
2017-11-28 17:39:24 -0500

    Added fadeIn/fadeOut

--- router.animations.ts
+++ router.animations.ts
@@ -36,3 +36,16 @@ export function moveInLeft() {
         ])
     ]);
 }
+
+export function fadeIn() {
+    return trigger('fadeIn', [
+        transition(':enter', [
+            style({ opacity: '0' }),
+            animate('1s .3s ease-in', style({ opacity: '1' }))
+        ]),
+        transition(':leave', [
+            style({ opacity: '1' }),
+            animate('1s .3s ease-in', style({ opacity: '0' }))
+        ])
+    ]);
+}
